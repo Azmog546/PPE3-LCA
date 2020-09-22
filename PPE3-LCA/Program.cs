e437aa42a81e0f41bc5c071149542dd9834e4f63
Alex <71330079+Azmog546@users.noreply.github.com>
2020-09-22 15:49:01 +0200

    Premiere synchro

--- PPE3-LCA/Program.cs
+++ PPE3-LCA/Program.cs
@@ -16,7 +16,7 @@ namespace PPE3_LCA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
     }
 }
