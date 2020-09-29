d909d9527bd247a7a9f947323a51e4b2f8e9c8e0
Alex <71330079+Azmog546@users.noreply.github.com>
2020-09-29 15:40:04 +0200

    Ajout page connexion

--- PPE3-LCA/Form1.Designer.cs
+++ PPE3-LCA/Form1.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connexionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seConnecterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connexionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connexionToolStripMenuItem
+            // 
+            this.connexionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.seConnecterToolStripMenuItem});
+            this.connexionToolStripMenuItem.Name = "connexionToolStripMenuItem";
+            this.connexionToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.connexionToolStripMenuItem.Text = "Connexion";
+            // 
+            // seConnecterToolStripMenuItem
+            // 
+            this.seConnecterToolStripMenuItem.Name = "seConnecterToolStripMenuItem";
+            this.seConnecterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.seConnecterToolStripMenuItem.Text = "se connecter";
+            this.seConnecterToolStripMenuItem.Click += new System.EventHandler(this.SeConnecterToolStripMenuItem_Click);
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Menu";
             this.Text = "GSB SWISS";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connexionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seConnecterToolStripMenuItem;
     }
 }
 
