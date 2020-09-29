c61dafce311e97991b91de3297bce86c86ca6415
Alex <71330079+Azmog546@users.noreply.github.com>
2020-09-29 14:58:49 +0200

    Joli page

---
+++ PPE3-LCA/FConnex.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PPE3_LCA
+{
+    partial class FConnex
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIdentifiant = new System.Windows.Forms.Label();
+            this.panelConnexion = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panelConnexion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblIdentifiant
+            // 
+            this.lblIdentifiant.AutoSize = true;
+            this.lblIdentifiant.Location = new System.Drawing.Point(68, 46);
+            this.lblIdentifiant.Name = "lblIdentifiant";
+            this.lblIdentifiant.Size = new System.Drawing.Size(53, 13);
+            this.lblIdentifiant.TabIndex = 0;
+            this.lblIdentifiant.Text = "Identifiant";
+            // 
+            // panelConnexion
+            // 
+            this.panelConnexion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panelConnexion.Controls.Add(this.textBox1);
+            this.panelConnexion.Controls.Add(this.lblIdentifiant);
+            this.panelConnexion.Location = new System.Drawing.Point(-11, 0);
+            this.panelConnexion.Name = "panelConnexion";
+            this.panelConnexion.Size = new System.Drawing.Size(817, 455);
+            this.panelConnexion.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // FConnex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelConnexion);
+            this.Name = "FConnex";
+            this.Text = "Connexion";
+            this.panelConnexion.ResumeLayout(false);
+            this.panelConnexion.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIdentifiant;
+        private System.Windows.Forms.Panel panelConnexion;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
--- PPE3-LCA/Modèle.cs
+++ PPE3-LCA/Modèle.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PPE3_LCA
 {
-    class Modèle
+    class Modele
     {
         private static LcaGsb maConnexion;
 
