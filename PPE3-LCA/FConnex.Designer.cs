7916c79c49346fc0442dc980c178c5499514c1b7
4clem4 <4clem4@gmail.com>
2020-10-13 14:43:05 +0000

    Connexion

--- PPE3-LCA/FConnex.Designer.cs
+++ PPE3-LCA/FConnex.Designer.cs
@@ -30,19 +30,19 @@
         {
             this.lblIdentifiant = new System.Windows.Forms.Label();
             this.panelConnexion = new System.Windows.Forms.Panel();
-            this.txtIdentifiant = new System.Windows.Forms.TextBox();
-            this.lblPassword = new System.Windows.Forms.Label();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.btnValider = new System.Windows.Forms.Button();
-            this.btnOk = new System.Windows.Forms.Button();
             this.lblEssai = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtIdentifiant = new System.Windows.Forms.TextBox();
             this.panelConnexion.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblIdentifiant
             // 
             this.lblIdentifiant.AutoSize = true;
-            this.lblIdentifiant.Location = new System.Drawing.Point(68, 46);
+            this.lblIdentifiant.Location = new System.Drawing.Point(38, 21);
             this.lblIdentifiant.Name = "lblIdentifiant";
             this.lblIdentifiant.Size = new System.Drawing.Size(53, 13);
             this.lblIdentifiant.TabIndex = 0;
@@ -60,49 +60,24 @@
             this.panelConnexion.Controls.Add(this.lblIdentifiant);
             this.panelConnexion.Location = new System.Drawing.Point(-11, 0);
             this.panelConnexion.Name = "panelConnexion";
-            this.panelConnexion.Size = new System.Drawing.Size(817, 455);
+            this.panelConnexion.Size = new System.Drawing.Size(433, 247);
             this.panelConnexion.TabIndex = 1;
             this.panelConnexion.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelConnexion_Paint);
             // 
-            // txtIdentifiant
+            // lblEssai
             // 
-            this.txtIdentifiant.BackColor = System.Drawing.Color.White;
-            this.txtIdentifiant.Location = new System.Drawing.Point(192, 43);
-            this.txtIdentifiant.Name = "txtIdentifiant";
-            this.txtIdentifiant.Size = new System.Drawing.Size(336, 20);
-            this.txtIdentifiant.TabIndex = 1;
-            // 
-            // lblPassword
-            // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(71, 92);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(71, 13);
-            this.lblPassword.TabIndex = 2;
-            this.lblPassword.Text = "Mot de passe";
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(192, 89);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '●';
-            this.txtPassword.Size = new System.Drawing.Size(336, 20);
-            this.txtPassword.TabIndex = 3;
-            // 
-            // btnValider
-            // 
-            this.btnValider.Location = new System.Drawing.Point(281, 383);
-            this.btnValider.Name = "btnValider";
-            this.btnValider.Size = new System.Drawing.Size(75, 23);
-            this.btnValider.TabIndex = 4;
-            this.btnValider.Text = "Valider";
-            this.btnValider.UseVisualStyleBackColor = true;
-            this.btnValider.Click += new System.EventHandler(this.BtnValider_Click);
+            this.lblEssai.AutoSize = true;
+            this.lblEssai.Location = new System.Drawing.Point(178, 135);
+            this.lblEssai.Name = "lblEssai";
+            this.lblEssai.Size = new System.Drawing.Size(35, 13);
+            this.lblEssai.TabIndex = 6;
+            this.lblEssai.Text = "label1";
+            this.lblEssai.Visible = false;
             // 
             // btnOk
             // 
             this.btnOk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.btnOk.Location = new System.Drawing.Point(381, 383);
+            this.btnOk.Location = new System.Drawing.Point(202, 175);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(75, 23);
             this.btnOk.TabIndex = 5;
@@ -110,23 +85,49 @@
             this.btnOk.UseVisualStyleBackColor = false;
             this.btnOk.Visible = false;
             // 
-            // lblEssai
+            // btnValider
             // 
-            this.lblEssai.AutoSize = true;
-            this.lblEssai.Location = new System.Drawing.Point(74, 168);
-            this.lblEssai.Name = "lblEssai";
-            this.lblEssai.Size = new System.Drawing.Size(35, 13);
-            this.lblEssai.TabIndex = 6;
-            this.lblEssai.Text = "label1";
-            this.lblEssai.Visible = false;
+            this.btnValider.Location = new System.Drawing.Point(102, 175);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(75, 23);
+            this.btnValider.TabIndex = 4;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.BtnValider_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(41, 76);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '●';
+            this.txtPassword.Size = new System.Drawing.Size(336, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(38, 60);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(71, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Mot de passe";
+            // 
+            // txtIdentifiant
+            // 
+            this.txtIdentifiant.BackColor = System.Drawing.Color.White;
+            this.txtIdentifiant.Location = new System.Drawing.Point(41, 37);
+            this.txtIdentifiant.Name = "txtIdentifiant";
+            this.txtIdentifiant.Size = new System.Drawing.Size(336, 20);
+            this.txtIdentifiant.TabIndex = 1;
             // 
             // FConnex
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(396, 234);
             this.Controls.Add(this.panelConnexion);
             this.Name = "FConnex";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Connexion";
             this.panelConnexion.ResumeLayout(false);
             this.panelConnexion.PerformLayout();
