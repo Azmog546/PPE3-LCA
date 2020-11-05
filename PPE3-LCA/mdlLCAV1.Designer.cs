7408b628b71ff891a674d40a499ad57363c8daea
LPERRIN <leoprn44@gmail.com>
2020-11-05 17:06:20 -0100

    5/11

--- PPE3-LCA/mdlLCAV1.Designer.cs
+++ PPE3-LCA/mdlLCAV1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\arioux\Source\Repos\PPE3-LCA\PPE3-LCA\mdlLCAV1.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\LPERRIN\Source\Repos\PPE3-LCA\PPE3-LCA\mdlLCAV1.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
